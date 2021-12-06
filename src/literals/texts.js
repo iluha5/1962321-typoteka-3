09fac9376d42b2aff277af36aeb6a822d561027b
Ilya Rovda <ilya.rovda@semrush.com>
2021-12-06 22:26:55 +0300

    add -v and -h commands

---
+++ src/literals/texts.js
@@ -0,0 +1,12 @@
+const HELP_OUTPUT =     ' Программа запускает http-сервер и формирует файл с данными для API.\n' +
+  '\n' +
+  '     Гайд:\n' +
+  '     service.js <command>\n' +
+  '     Команды:\n' +
+  '      --version: выводит номер версии\n' +
+  '      --help: печатает этот текст\n' +
+  '      --generate <count> формирует файл mocks.json';
+
+module.exports = {
+  HELP_OUTPUT,
+}
--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,38 @@
 'use strict';
 
-console.log(`Hello, world!`);
+import {HELP_OUTPUT} from "../literals/texts";
+
+const { Command } = require('commander');
+const fs = require(`fs`);
+
+const packageJsonFile = require(`../../package.json`);
+
+const ARGUMENTS = {
+  VERSION: 'version',
+  HELP: 'help',
+}
+
+const program = new Command();
+
+program
+  .option(`-v, --${ARGUMENTS.VERSION}`, 'output the program version')
+  .option(`-h, --${ARGUMENTS.HELP}`, 'output the program version')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options[ARGUMENTS.VERSION]) {
+  const version = packageJsonFile.version;
+  console.log(version);
+}
+
+if (options[ARGUMENTS.HELP]) {
+  console.log(HELP_OUTPUT);
+}
+
+// if (options.debug) console.log(options);
+// console.log('pizza details:');
+// if (options.small) console.log('- small pizza size');
+// if (options.pizzaType) console.log(`- ${options.pizzaType}`);
