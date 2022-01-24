bdedca1d2ca3ff74f38ed83e9e5eb5fe0e991f57
Ilya Rovda <ilya.rovda@semrush.com>
2022-01-24 19:29:01 +0300

    fix static paths, fix main page links, add 404 render

--- src/express/express.js
+++ src/express/express.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require(`express`);
 const path = require(`path`);
+const statusCodes = require(`http-status`);
 const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
 const articlesRouter = require(`./routes/articles-routes`);
@@ -8,14 +9,18 @@ const {DEFAULT_EXPRESS_PORT, PUBLIC_DIR} = require(`../service/config`);
 
 const app = express();
 
+app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
-app.set(`views`, path.join(__dirname, `templates`));
 
-// TODO: doesn't work for nested routes
-app.use(express.static(path.join(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/articles`, articlesRouter);
 app.use(`/my`, myRouter);
 app.use(`/`, mainRouter);
+
+app.get(`*`, (req, res) => {
+  res.status(statusCodes.NOT_FOUND).render(`404`);
+});
+
 
 app.listen(DEFAULT_EXPRESS_PORT);
