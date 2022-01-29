c26848a12feeee2e25eaac37ced09f2d76167bca
Ilya Rovda <ilya.rovda@semrush.com>
2022-01-29 11:15:00 +0300

    ref service server, add posts routex

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,49 +2,24 @@
 
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
-const http = require(`http`);
-const statusCodes = require(`http-status`);
+const express = require(`express`);
 const {ARGUMENTS, DEFAULT_PORT, MOCK_FILE_NAME} = require(`../config`);
-const {SERVER_STARTED, WRONG_PORT, SERVER_STARTING_ERROR, PAGE_TITLE} = require(`../../literals/texts`);
+const {SERVER_STARTED, WRONG_PORT, SERVER_STARTING_ERROR} = require(`../../literals/texts`);
 const {getIsInteger} = require(`../../helpers`);
 
-const CONTENT_TYPE = `text/html; charset=UTF-8`;
+const sendPosts = async (req, res) => {
+  let mockFileContent = [];
 
-const sendResponse = (res, status, message) => {
-  const template = `
-    <!Doctype html>
-    <html lang="ru">
-      <head>
-        <title>${PAGE_TITLE}</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
+  try {
+    const mockFile = await fs.readFile(MOCK_FILE_NAME);
 
-  res.writeHead(status, {
-    'Content-Type': CONTENT_TYPE,
-  });
+    mockFileContent = JSON.parse(mockFile);
 
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(MOCK_FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-
-        sendResponse(res, statusCodes.OK, `<ul>${message}</ul>`);
-      } catch (error) {
-        console.error(chalk.red(error));
-
-        sendResponse(res, statusCodes.NOT_FOUND, statusCodes[statusCodes.NOT_FOUND]);
-      }
-      break;
-    default:
-      sendResponse(res, statusCodes.NOT_FOUND, statusCodes[statusCodes.NOT_FOUND]);
+  } catch (error) {
+    console.error(chalk.red(error));
   }
+
+  res.send(mockFileContent);
 };
 
 const run = (port) => {
@@ -56,14 +31,17 @@ const run = (port) => {
     console.error(chalk.red(WRONG_PORT));
   }
 
-  http.createServer(onClientConnect)
-      .listen(currentPort)
-      .on(`listening`, () => {
-        console.log(chalk.green(`${SERVER_STARTED}: ${currentPort}`));
-      })
-      .on(`error`, ({message}) => {
-        console.error(chalk.red(SERVER_STARTING_ERROR, message));
-      });
+  const app = express();
+
+  app.use(express.json());
+
+  app.get(`/posts`, sendPosts);
+
+  app.listen(currentPort).on(`error`, (message) => {
+    console.error(chalk.red(SERVER_STARTING_ERROR, message));
+    process.exit(1);
+  });
+  console.log(chalk.green(`${SERVER_STARTED}: ${currentPort}`));
 };
 
 module.exports = {
