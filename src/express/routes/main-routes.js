4cba708ef1bad86904784fa22f49b9d39bfe6331
Ilya Rovda <ilya.rovda@semrush.com>
2022-02-25 19:45:20 +0300

    add api class, fix main template

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const {Router} = require(`express`);
+const {getAPI} = require(`../api`);
 
+const api = getAPI();
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => {
-  res.render(`main`);
+mainRouter.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+
+  // TODO: add lastComments and popularArticles calculation and pass to main
+  res.render(`main`, {articles});
 });
 mainRouter.get(`/register`, (req, res) => {
   res.render(`sign-up`);
