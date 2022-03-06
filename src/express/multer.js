f36add7f020d06edd7cd5fc90013e6e7fcfdf7c5
Ilya Rovda <ilya.rovda@semrush.com>
2022-03-06 22:58:37 +0300

    fix file uploading

--- src/express/multer.js
+++ src/express/multer.js
@@ -9,7 +9,7 @@ const UPLOAD_DIR = `./upload/img/`;
 const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
 
 const storage = multer.diskStorage({
-  dest: uploadDirAbsolute,
+  destination: uploadDirAbsolute,
   filename: (req, file, cb) => {
     const uniqueName = nanoid(10);
     const extension = file.originalname.split(`.`).pop();
